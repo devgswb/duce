bb7376ac048c012c8023a629c57c3b114dda459f
GoatHead <erpsm92@gmail.com>
2019-03-16 13:28:42 +0900

    demo init

---
+++ src/main/java/kr/ac/duce/ducexpo/DemoApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.duce.ducexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
