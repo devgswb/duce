dfc1b14c8e14d5cf31a36013038aa7dd3b86d9b0
Seong Min Joe <erpsm92@gmail.com>
2019-06-07 17:59:26 +0900

    log off

--- src/main/java/kr/ac/duce/service/impl/IntroSettingServiceImpl.java
+++ src/main/java/kr/ac/duce/service/impl/IntroSettingServiceImpl.java
@@ -35,7 +35,6 @@ public class IntroSettingServiceImpl implements IntroSettingService {
         JsonReader reader = new JsonReader(new FileReader(jsonpath));
         JsonParser parser = new JsonParser();
         JsonObject jsonObj = (JsonObject) parser.parse(reader);
-        System.out.println(introForm.toString());
         if(!file.isEmpty()) {
             String formerImg = jsonObj.get("img").toString();
             formerImg = formerImg.replace("\"", "");
